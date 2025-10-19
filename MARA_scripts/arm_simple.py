e358552167a16fe6f85d964a65347dd5774c5845
soerensenkarl <soerensenkarl@gmail.com>
2025-10-19 19:33:37 +0200

    sequence now working, chaining together autowalk and arm movement

--- MARA_scripts/arm_simple.py
+++ MARA_scripts/arm_simple.py
@@ -1,184 +1,85 @@
-# Copyright (c) 2023 Boston Dynamics, Inc.  All rights reserved.
-#
-# Downloading, reproducing, distributing or otherwise using the SDK Software
-# is subject to the terms and conditions of the Boston Dynamics Software
-# Development Kit License (20191101-BDSDK-SL).
-
-
-"""Tutorial to show how to use Spot's arm.
-"""
-
-import argparse
-import sys
+# arm_simple.py
 import time
-
-import bosdyn.api.gripper_command_pb2
-import bosdyn.client
-import bosdyn.client.lease
-import bosdyn.client.util
 from bosdyn.api import arm_command_pb2, geometry_pb2
 from bosdyn.client import math_helpers
-from bosdyn.client.frame_helpers import GRAV_ALIGNED_BODY_FRAME_NAME, ODOM_FRAME_NAME, get_a_tform_b
-from bosdyn.client.robot_command import (RobotCommandBuilder, RobotCommandClient,
-                                         block_until_arm_arrives, blocking_stand)
+from bosdyn.client.frame_helpers import (
+    GRAV_ALIGNED_BODY_FRAME_NAME,
+    ODOM_FRAME_NAME,
+    get_a_tform_b,
+)
+from bosdyn.client.robot_command import (
+    RobotCommandBuilder,
+    RobotCommandClient,
+    block_until_arm_arrives,
+)
 from bosdyn.client.robot_state import RobotStateClient
 
 
-def hello_arm(config):
-    """A simple example of using the Boston Dynamics API to command Spot's arm."""
-
-    # See hello_spot.py for an explanation of these lines.
-    bosdyn.client.util.setup_logging(config.verbose)
-
-    sdk = bosdyn.client.create_standard_sdk('HelloSpotClient')
-    robot = sdk.create_robot(config.hostname)
-    bosdyn.client.util.authenticate(robot)
-    robot.time_sync.wait_for_sync()
-
-    assert robot.has_arm(), 'Robot requires an arm to run this example.'
-
-    # Verify the robot is not estopped and that an external application has registered and holds
-    # an estop endpoint.
-    assert not robot.is_estopped(), 'Robot is estopped. Please use an external E-Stop client, ' \
-                                    'such as the estop SDK example, to configure E-Stop.'
-
-    robot_state_client = robot.ensure_client(RobotStateClient.default_service_name)
-
-    lease_client = robot.ensure_client(bosdyn.client.lease.LeaseClient.default_service_name)
-    with bosdyn.client.lease.LeaseKeepAlive(lease_client, must_acquire=True, return_at_exit=True):
-        # Now, we are ready to power on the robot. This call will block until the power
-        # is on. Commands would fail if this did not happen. We can also check that the robot is
-        # powered at any point.
-        robot.logger.info('Powering on robot... This may take a several seconds.')
-        robot.power_on(timeout_sec=20)
-        assert robot.is_powered_on(), 'Robot power on failed.'
-        robot.logger.info('Robot powered on.')
-
-        # Tell the robot to stand up. The command service is used to issue commands to a robot.
-        # The set of valid commands for a robot depends on hardware configuration. See
-        # RobotCommandBuilder for more detailed examples on command building. The robot
-        # command service requires timesync between the robot and the client.
-        robot.logger.info('Commanding robot to stand...')
-        command_client = robot.ensure_client(RobotCommandClient.default_service_name)
-        blocking_stand(command_client, timeout_sec=10)
-        robot.logger.info('Robot standing.')
-
-        # Move the arm to a spot in front of the robot, and open the gripper.
-
-        # Make the arm pose RobotCommand
-        # Build a position to move the arm to (in meters, relative to and expressed in the gravity aligned body frame).
-        x = 0.75
-        y = 0
-        z = 0.25
-        hand_ewrt_flat_body = geometry_pb2.Vec3(x=x, y=y, z=z)
-
-        # Rotation as a quaternion
-        qw = 1
-        qx = 0
-        qy = 0
-        qz = 0
-        flat_body_Q_hand = geometry_pb2.Quaternion(w=qw, x=qx, y=qy, z=qz)
-
-        flat_body_T_hand = geometry_pb2.SE3Pose(position=hand_ewrt_flat_body,
-                                                rotation=flat_body_Q_hand)
-
-        robot_state = robot_state_client.get_robot_state()
-        odom_T_flat_body = get_a_tform_b(robot_state.kinematic_state.transforms_snapshot,
-                                         ODOM_FRAME_NAME, GRAV_ALIGNED_BODY_FRAME_NAME)
-
-        odom_T_hand = odom_T_flat_body * math_helpers.SE3Pose.from_proto(flat_body_T_hand)
-
-        # duration in seconds
-        seconds = 2
-
-        arm_command = RobotCommandBuilder.arm_pose_command(
-            odom_T_hand.x, odom_T_hand.y, odom_T_hand.z, odom_T_hand.rot.w, odom_T_hand.rot.x,
-            odom_T_hand.rot.y, odom_T_hand.rot.z, ODOM_FRAME_NAME, seconds)
-
-        # Make the open gripper RobotCommand
-        gripper_command = RobotCommandBuilder.claw_gripper_open_fraction_command(1.0)
-
-        # Combine the arm and gripper commands into one RobotCommand
-        command = RobotCommandBuilder.build_synchro_command(gripper_command, arm_command)
-
-        # Send the request
-        cmd_id = command_client.robot_command(command)
-        robot.logger.info('Moving arm to position 1.')
-
-        # Wait until the arm arrives at the goal.
-        block_until_arm_arrives_with_prints(robot, command_client, cmd_id)
-
-        # Move the arm to a different position
-        hand_ewrt_flat_body.z = 0
-
-        flat_body_Q_hand.w = 0.707
-        flat_body_Q_hand.x = 0.707
-        flat_body_Q_hand.y = 0
-        flat_body_Q_hand.z = 0
-
-        flat_body_T_hand2 = geometry_pb2.SE3Pose(position=hand_ewrt_flat_body,
-                                                 rotation=flat_body_Q_hand)
-        odom_T_hand = odom_T_flat_body * math_helpers.SE3Pose.from_proto(flat_body_T_hand2)
-
-        arm_command = RobotCommandBuilder.arm_pose_command(
-            odom_T_hand.x, odom_T_hand.y, odom_T_hand.z, odom_T_hand.rot.w, odom_T_hand.rot.x,
-            odom_T_hand.rot.y, odom_T_hand.rot.z, ODOM_FRAME_NAME, seconds)
-
-        # Close the gripper
-        gripper_command = RobotCommandBuilder.claw_gripper_open_fraction_command(0.0)
-
-        # Build the proto
-        command = RobotCommandBuilder.build_synchro_command(gripper_command, arm_command)
-
-        # Send the request
-        cmd_id = command_client.robot_command(command)
-        robot.logger.info('Moving arm to position 2.')
-
-        # Wait until the arm arrives at the goal.
-        # Note: here we use the helper function provided by robot_command.
-        block_until_arm_arrives(command_client, cmd_id)
-
-        robot.logger.info('Done.')
-
-        # Power the robot off. By specifying "cut_immediately=False", a safe power off command
-        # is issued to the robot. This will attempt to sit the robot before powering off.
-        robot.power_off(cut_immediately=False, timeout_sec=20)
-        assert not robot.is_powered_on(), 'Robot power off failed.'
-        robot.logger.info('Robot safely powered off.')
-
-
-def block_until_arm_arrives_with_prints(robot, command_client, cmd_id):
-    """Block until the arm arrives at the goal and print the distance remaining.
-        Note: a version of this function is available as a helper in robot_command
-        without the prints.
+def run(robot, seconds: float = 2.0):
     """
-    while True:
-        feedback_resp = command_client.robot_command_feedback(cmd_id)
-        measured_pos_distance_to_goal = feedback_resp.feedback.synchronized_feedback.arm_command_feedback.arm_cartesian_feedback.measured_pos_distance_to_goal
-        measured_rot_distance_to_goal = feedback_resp.feedback.synchronized_feedback.arm_command_feedback.arm_cartesian_feedback.measured_rot_distance_to_goal
-        robot.logger.info('Distance to go: %.2f meters, %.2f radians',
-                          measured_pos_distance_to_goal, measured_rot_distance_to_goal)
+    Simple two-pose arm move with open/close gripper, then STOW.
+    PRECONDITIONS (handled in sequence.py):
+      - robot is authenticated & time-synced
+      - lease keepalive is active
+      - robot is powered on AND standing
+    """
+    robot_state_client = robot.ensure_client(RobotStateClient.default_service_name)
+    command_client = robot.ensure_client(RobotCommandClient.default_service_name)
 
-        if feedback_resp.feedback.synchronized_feedback.arm_command_feedback.arm_cartesian_feedback.status == arm_command_pb2.ArmCartesianCommand.Feedback.STATUS_TRAJECTORY_COMPLETE:
-            robot.logger.info('Move complete.')
+    # --- Pose 1: forward & a bit up, gripper open ---
+    x, y, z = 0.75, 0.0, 0.25
+    flat_pose = geometry_pb2.SE3Pose(
+        position=geometry_pb2.Vec3(x=x, y=y, z=z),
+        rotation=geometry_pb2.Quaternion(w=1.0, x=0.0, y=0.0, z=0.0),
+    )
+
+    robot_state = robot_state_client.get_robot_state()
+    odom_T_flat = get_a_tform_b(
+        robot_state.kinematic_state.transforms_snapshot,
+        ODOM_FRAME_NAME,
+        GRAV_ALIGNED_BODY_FRAME_NAME,
+    )
+    odom_T_hand = odom_T_flat * math_helpers.SE3Pose.from_proto(flat_pose)
+
+    cmd1 = RobotCommandBuilder.build_synchro_command(
+        RobotCommandBuilder.claw_gripper_open_fraction_command(1.0),
+        RobotCommandBuilder.arm_pose_command(
+            odom_T_hand.x, odom_T_hand.y, odom_T_hand.z,
+            odom_T_hand.rot.w, odom_T_hand.rot.x, odom_T_hand.rot.y, odom_T_hand.rot.z,
+            ODOM_FRAME_NAME, seconds,
+        ),
+    )
+    cmd_id = command_client.robot_command(cmd1)
+    _block_until_cartesian_done(robot, command_client, cmd_id)
+
+    # --- Pose 2: lower & rotate a bit, gripper close ---
+    flat_pose.position.z = 0.0
+    flat_pose.rotation.w, flat_pose.rotation.x, flat_pose.rotation.y, flat_pose.rotation.z = 0.707, 0.707, 0.0, 0.0
+    odom_T_hand2 = odom_T_flat * math_helpers.SE3Pose.from_proto(flat_pose)
+
+    cmd2 = RobotCommandBuilder.build_synchro_command(
+        RobotCommandBuilder.claw_gripper_open_fraction_command(0.0),
+        RobotCommandBuilder.arm_pose_command(
+            odom_T_hand2.x, odom_T_hand2.y, odom_T_hand2.z,
+            odom_T_hand2.rot.w, odom_T_hand2.rot.x, odom_T_hand2.rot.y, odom_T_hand2.rot.z,
+            ODOM_FRAME_NAME, seconds,
+        ),
+    )
+    cmd_id = command_client.robot_command(cmd2)
+    block_until_arm_arrives(command_client, cmd_id)
+
+    # --- STOW the arm (brief wait to retract) ---
+    stow_cmd = RobotCommandBuilder.arm_stow_command()
+    command_client.robot_command(stow_cmd)
+    time.sleep(1.0)
+
+    robot.logger.info("arm_simple.run() complete (stowed).")
+
+
+def _block_until_cartesian_done(robot, command_client, cmd_id):
+    while True:
+        fb = command_client.robot_command_feedback(cmd_id)
+        cart = fb.feedback.synchronized_feedback.arm_command_feedback.arm_cartesian_feedback
+        if cart.status == arm_command_pb2.ArmCartesianCommand.Feedback.STATUS_TRAJECTORY_COMPLETE:
             break
         time.sleep(0.1)
-
-
-def main():
-    """Command line interface."""
-    parser = argparse.ArgumentParser()
-    bosdyn.client.util.add_base_arguments(parser)
-    options = parser.parse_args()
-    try:
-        hello_arm(options)
-        return True
-    except Exception as exc:  # pylint: disable=broad-except
-        logger = bosdyn.client.util.get_logger()
-        logger.exception('Threw an exception')
-        return False
-
-
-if __name__ == '__main__':
-    if not main():
-        sys.exit(1)
--- MARA_scripts/sequence.py
+++ MARA_scripts/sequence.py
@@ -1,21 +1,63 @@
-import os
-import subprocess
+# sequence.py
 import time
+from pathlib import Path  # ← ADD THIS LINE
 
-HOST = "192.168.80.3"      # your Spot IP
-USER = "user"             # your username
-PASS = "password"     # your password
+import bosdyn.client
+import bosdyn.client.util
+from bosdyn.client.lease import LeaseClient, LeaseKeepAlive
+from bosdyn.client.robot_command import RobotCommandClient, blocking_stand
+import arm_simple
 
-def run_module(modname):
-    print(f"\n=== Running {modname} ===")
-    env = os.environ.copy()
-    env["BOSDYN_CLIENT_USERNAME"] = USER
-    env["BOSDYN_CLIENT_PASSWORD"] = PASS
-    # If you also use certs/app tokens, set env here too.
-    subprocess.run(["py", "-m", modname, HOST], env=env, check=True)
+# ---- Set once here (or swap to env vars) ----
+HOST = "192.168.80.3"
+
+# Path to credentials file
+CRED_PATH = Path(r"C:\Users\soere\OneDrive\Desktop\spot_creds.txt")  # change this if needed
+
+def load_credentials(path: Path):
+    """Read username/password from a two-line text file."""
+    with path.open("r") as f:
+        lines = [line.strip() for line in f.readlines() if line.strip()]
+    if len(lines) < 2:
+        raise ValueError(f"Credential file {path} must contain at least two lines: USER and PASS")
+    return lines[0], lines[1]
+
+
+def main():
+    USER, PASS = load_credentials(CRED_PATH)
+    
+    sdk = bosdyn.client.create_standard_sdk("MARA_Sequence")
+    robot = sdk.create_robot(HOST)
+    robot.authenticate(USER, PASS)
+    robot.time_sync.wait_for_sync()
+
+    assert not robot.is_estopped(), "Robot is E-Stopped. Configure external E-Stop first."
+    assert robot.has_arm(), "This sequence expects a Spot with an arm."
+
+    lease_client = robot.ensure_client(LeaseClient.default_service_name)
+    with LeaseKeepAlive(lease_client, must_acquire=True, return_at_exit=True):
+        # Power on & STAND ONCE for the entire sequence
+        robot.logger.info("Powering on…")
+        robot.power_on(timeout_sec=20)
+        assert robot.is_powered_on(), "Power on failed."
+
+        cmd_client = robot.ensure_client(RobotCommandClient.default_service_name)
+        robot.logger.info("Standing…")
+        blocking_stand(cmd_client, timeout_sec=10)
+
+        # ---- Call actions (they assume standing & ready) ----
+        robot.logger.info("Action 1: arm_simple")
+        arm_simple.run(robot, seconds=2.0)
+
+        robot.logger.info("Action 2: arm_simple (again)")
+        arm_simple.run(robot, seconds=2.0)
+
+        # ---- Finish: safe sit + power off ----
+        robot.logger.info("Sequence complete. Powering off (safe sit)…")
+        robot.power_off(cut_immediately=False, timeout_sec=20)
+
+    robot.logger.info("Lease returned. Done.")
+
 
 if __name__ == "__main__":
-    run_module("arm_simple")
-    time.sleep(2)
-    run_module("arm_simple2")
-    print("\nSequence complete.")
+    main()
